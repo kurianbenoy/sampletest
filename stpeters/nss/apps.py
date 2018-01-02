ffc9ad18acb5d552eaaaf502b88438f33f8885a4
kurianbenoy <kurian.pro@gmail.com>
2018-01-03 02:27:07 +0530

    add images and hopefully this solves git issues

--- stpeters/nss/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NssConfig(AppConfig):
-    name = 'nss'
--- stpeters/nss/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import NssUpload
-
-class NssUploadForm(forms.ModelForm):
-	class Meta:
-		model = NssUpload
-		fields = ['title','description']
